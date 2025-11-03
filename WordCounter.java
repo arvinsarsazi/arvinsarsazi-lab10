9ced490ea5bbff3dffc0a6ba36667c903db2cac1
Arvin Sarsazi <arvin@Arvins-MacBook-Pro.local>
2025-11-02 21:59:11 -0500

    ProcessText Done

--- WordCounter.java
+++ WordCounter.java
@@ -5,10 +5,38 @@ public class WordCounter{
         
     }
     public static int processText(StringBuffer input, String y) throws InvalidStopwordException, TooSmallText {
-        return 0;
+        Pattern p = Pattern.compile("[A-Za-z0-9']+");
+        Marcher m = p.matcher(input);
+        int count = 0;
+        boolean stopwordfound = false;
+        
+        while(m.find() == true) {
+            String word = m.group();
+            count++;
+
+            if (stopwordfound == false) {
+                if (word.equals(stopword)) {
+                    stopwordfound = true;
+                    break;
+                }
+            }
+        }
+        if (count < 5) {
+            throw new TooSmallText("Only found " + count + " words.");
+        }
+
+        if (target != null && !foundStop) {
+            throw new InvalidStopwordException("Couldn't find stopword: " + stopword);
+        }
+
+        return count;
     }
-    public StringBuffer processFile(String y) throws EmptyFileException{
+    public static StringBuffer processFile(String y) throws EmptyFileException{
 
         return null;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
